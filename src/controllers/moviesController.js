bdf14a53208b87b4a0381f54da5b55c08d6c235d
LUCAS\monto <lucasmontoby@gmail.com>
2024-04-11 21:14:38 -0300

    Modificar

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,9 +1,12 @@
 const db = require('../database/models');
 const path = require('path')
+const moment = require('moment');
 
 
+//Acá tienen otra forma de llamar a cada uno de los modelos
 const Movies = db.Movie;
 const Genres = db.Genre;
+
 
 const moviesController = {
     'list': (req, res) => {
@@ -17,77 +20,72 @@ const moviesController = {
 
     'detail': (req, res) => {
         db.Movie.findByPk(req.params.id,
-        {
-            include: ['genre']
-        })
-        .then(movie => {
-            res.render('moviesDetail.ejs', {movie})
-        })
+            {
+                include : ['genre']
+            })
+            .then(movie => {
+                res.render('moviesDetail.ejs', {movie});
+            });
     },
 
-                    // CRUD
 
-                        add: function (req, res) {
-                            let promGenres = Genres.findAll();
-                        
-                            Promise.all([promGenres])
-                            .then(([allGenres]) => {
-                                return res.render(path.resolve(__dirname, '..', 'views', 'moviesAdd'), { allGenres });
-                            })
-                            .catch(error => {
-                                console.error('Error al buscar géneros:', error);
-                                return res.status(500).send('Error interno del servidor');
-                            });
-                        }, 
-                        
-                        create: function (req, res) {
-                            Movies.create({
-                                title: req.body.title,
-                                rating: req.body.rating,
-                                awards: req.body.awards,
-                                release_date: req.body.release_date,
-                                length: req.body.length,
-                                genre_id: req.body.genre_id
-                            })
-                            .then(() => {
-                                return res.redirect('/movies');
-                            })
-                            .catch(error => {
-                                console.error('Error al crear película:', error);
-                                return res.status(500).send('Error interno del servidor');
-                            });
-                        },
+    //Acá dispongo las rutas para trabajar con el CRUD
+    add: function (req, res) {
+        let promGenres = Genres.findAll();
+        
+        Promise
+        .all([promGenres])
+        .then(([allGenres]) => {
+            return res.render(path.resolve(__dirname, '..', 'views',  'moviesAdd'), {allGenres})})
+        .catch(error => res.send(error))
+    },
 
-                        edit: function(req,res) {
-                            let movieId = req.params.id;
-                            let promMovies = Movies.findByPk(movieId,{include: ['genre']});
-                            let promGenres = Genres.findAll();
-                            Promise
-                            .all([promMovies, promGenres])
-                            .then(([Movie, allGenres]) => {
-                                Movie.release_date = moment(Movie.release_date).format('L');
-                                return res.render(path.resolve(__dirname, '..', 'views',  'moviesEdit'), {Movie,allGenres})})
-                            .catch(error => res.send(error))
-                        },
-                    
-                        update: function (req,res) {
-                            let movieId = req.params.id;
-                            Movies.update(
-                                {
-                                    title: req.body.title,
-                                    rating: req.body.rating,
-                                    awards: req.body.awards,
-                                    release_date: req.body.release_date,
-                                    length: req.body.length,
-                                    genre_id: req.body.genre_id
-                                },
-                                {
-                                    where: {id: movieId}
-                                })
-                            .then(()=> {
-                                return res.redirect('/movies')})            
-                            .catch(error => res.send(error))
-                        }
+    create: function (req,res) {
+        Movies.create(
+            {
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length,
+                genre_id: req.body.genre_id
+            }
+        )
+        .then(()=> {
+            return res.redirect('/movies')})
+        .catch(error => res.send(error))
+    },
+
+    edit: function(req,res) {
+        let movieId = req.params.id;
+        let promMovies = Movies.findByPk(movieId,{include: ['genre']});
+        let promGenres = Genres.findAll();
+        Promise
+        .all([promMovies, promGenres])
+        .then(([Movie, allGenres]) => {
+            Movie.release_date = moment(Movie.release_date).format('L');
+            return res.render(path.resolve(__dirname, '..', 'views',  'moviesEdit'), {Movie,allGenres})})
+        .catch(error => res.send(error))
+    },
+
+    update: function (req,res) {
+        let movieId = req.params.id;
+        Movies.update(
+            {
+                title: req.body.title,
+                rating: req.body.rating,
+                awards: req.body.awards,
+                release_date: req.body.release_date,
+                length: req.body.length,
+                genre_id: req.body.genre_id
+            },
+            {
+                where: {id: movieId}
+            })
+        .then(()=> {
+            return res.redirect('/movies')})            
+        .catch(error => res.send(error))
+    }
                     
                                 
 }
--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const moviesController = require("../controllers/moviesController")
 
 router.get('/movies', moviesController.list);
-router.get('/movies/detail/:id', moviesController.detail);
-router.get('/movies/add', moviesController.add);
-router.post('/movies/create', moviesController.create);
+router.get('/movies/detail/:id', moviesController.detail)
+router.get("/movies/add", moviesController.add)
+router.post("/movies/create", moviesController.create)
 router.get('/movies/edit/:id', moviesController.edit);
-router.put("/movies/update/:id", moviesController.update);
+router.put("/movies/update/:id", moviesController.update)
+
 
 module.exports = router;
